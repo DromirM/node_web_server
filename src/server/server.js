26e5b1f4ef54ca5514566573623150f2b5f4056d
Elias_B <DromirM@users.noreply.github.com>
2025-03-10 17:28:48 -0300

    Primer commit del  servidor web.

---
+++ src/server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const startServer = (options) => {
+  //Desestructuro el parametro.
+  const {port, public_path = 'public'} = options;
+  
+  const app = express();
+
+  // Para poder usar middlewares se usa la palabra use (express)
+  app.use(express.static(public_path)); // Contenido estatico que ponemos disponible.
+
+  app.get('*', (req, res) => {
+    //Esta configuracion es muy util para single page aplications.
+    const indexPath = path.join(__dirname + `../../../${public_path}/index.html`);
+    res.sendFile(indexPath);
+  });
+  
+  //Permite abrir un puerto y escuchar por el.
+  app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+
+  });
+};
+
+
+module.exports = {
+  startServer
+}
